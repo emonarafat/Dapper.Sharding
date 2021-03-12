0f4d8ec369e9c9d1baaa5e4f37a97239cccf84f0
znyet <821023977@qq.com>
2021-03-12 13:34:00 +0800

    1

--- src/Dapper.Sharding/Entity/DistributedTransaction.cs
+++ src/Dapper.Sharding/Entity/DistributedTransaction.cs
@@ -12,72 +12,65 @@ namespace Dapper.Sharding
 
         private Dictionary<object, object> dict = new Dictionary<object, object>();
 
-        private object _lock = new object();
-
         public int? CommandTimeout = null;
 
         public ITable<T> GetTranTable<T>(ITable<T> table) where T : class
         {
-            if (!dict.ContainsKey(table))
+            var ok = dict.TryGetValue(table, out var val);
+            if (!ok)
             {
-                lock (_lock)
+                #region database conn tran cache
+
+                var db = table.DataBase;
+                IDbConnection conn;
+                IDbTransaction tran;
+
+                //不存在此db的conn进行添加
+                if (!dictConn.ContainsKey(db))
                 {
-                    if (!dict.ContainsKey(table))
+                    conn = db.GetConn();
+                    tran = conn.BeginTransaction();
+                    dictConn.Add(db, conn);
+                    dictTran.Add(db, tran);
+                }
+                else //直接取出来
+                {
+                    conn = dictConn[db];
+                    tran = dictTran[db];
+                }
+
+                #endregion
+
+                try
+                {
+                    val = table.CreateTranTable(conn, tran, CommandTimeout);
+                    dict.Add(table, val);
+                }
+                catch (Exception ex)
+                {
+                    if (conn != null && conn.State == ConnectionState.Open)
                     {
-                        #region database conn tran cache
-
-                        var db = table.DataBase;
-                        IDbConnection conn = null;
-                        IDbTransaction tran = null;
-
-                        //不存在此db的conn进行添加
-                        if (!dictConn.ContainsKey(db))
-                        {
-                            conn = db.GetConn();
-                            tran = conn.BeginTransaction();
-                            dictConn.Add(db, conn);
-                            dictTran.Add(db, tran);
-                        }
-                        else //直接取出来
-                        {
-                            conn = dictConn[db];
-                            tran = dictTran[db];
-                        }
-
-                        #endregion
-
                         try
                         {
-                            ITable<T> tranTable = table.CreateTranTable(conn, tran, CommandTimeout);
-                            dict.Add(table, tranTable);
+                            conn.Dispose();
                         }
-                        catch (Exception ex)
-                        {
-                            if (conn != null && conn.State == ConnectionState.Open)
-                            {
-                                try
-                                {
-                                    conn.Dispose();
-                                }
-                                catch { }
+                        catch { }
 
-                            }
-                            if (tran != null)
-                            {
-                                try
-                                {
-                                    tran.Dispose();
-                                }
-                                catch { }
-
-                            }
-                            Close();
-                            throw ex;
-                        }
                     }
+                    if (tran != null)
+                    {
+                        try
+                        {
+                            tran.Dispose();
+                        }
+                        catch { }
+
+                    }
+                    Close();
+                    throw ex;
                 }
             }
-            return (ITable<T>)dict[table];
+            return (ITable<T>)val;
         }
 
         private void Close()
